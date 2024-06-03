911ad89407d821f3cace4429eac989d1b172b06d
dalramjwi <dalramjwi@gmail.com>
2024-06-03 11:26:52 +0900

    객체 key 조회 명령어를 알고 시도함. 성공

--- work-1.js
+++ work-1.js
@@ -4,5 +4,19 @@
 function workOne(a, b) {
   //* logic : 매개변수 두 개를 받은 다음
   //* logic : return은 객체
-  return 객체;
+  let localVariable = {};
+  localVariable = { [a]: b };
+  /**동료의 방법
+   * localVariable[a] = b;
+   */
+  localVariable = {
+    a: a,
+    b: b,
+  };
+  return localVariable;
 }
+oj = {
+  키: "value",
+};
+console.log(workOne("객체 key", "객체 value"));
+console.log(Object.keys(oj));
