820637331141ee86ef510aafc7da596724768928
dalramjwi <dalramjwi@gmail.com>
2024-06-03 14:50:42 +0900

    함수도 사용해보았다. 아닌 경우 시험

--- constructor.js
+++ constructor.js
@@ -11,6 +11,11 @@ class ramen {
       (this.ramenWater = ramenWater),
       (this.addFood = addFood);
   }
+  set ramenName(ramenName) {
+    if (ramenName !== "진순") {
+      console.error("올바른 이름이 아닙니다.");
+    }
+  }
 }
-let test = new ramen("진순", "물", "치즈");
+let test = new ramen("진매", "물", "치즈");
 console.log(test);
