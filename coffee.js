41b8cb84cfb853ab455f6d654f428c61d9ac81a2
dalramjwi <dalramjwi@gmail.com>
2024-06-03 15:46:53 +0900

    문자열이라는 조건 세팅, 세팅된 값은 언더바를 앞에 붙이는 관습 존재

--- coffee.js
+++ coffee.js
@@ -5,6 +5,14 @@ class makeCoffee {
     this.sugar = sugar;
     this.milk = milk;
   }
+  //* 규격 만들기 : constructor 밖 class 안
+  set wondoo(value) {
+    if (typeof value === "string") {
+      this._won = value;
+    } else {
+      console.error("문자열 아님");
+    }
+  }
 }
 let test = new makeCoffee("원두", "물", "설탕", "우유");
 console.log(test);
