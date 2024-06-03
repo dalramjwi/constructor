225e3b9948dcabd354860b19e8fcfe6814a2fb3c
dalramjwi <dalramjwi@gmail.com>
2024-06-03 15:40:40 +0900

    class 한번 더 써보기

---
+++ coffee.js
@@ -0,0 +1,10 @@
+class makeCoffee {
+  constructor(wondoo, water, sugar, milk) {
+    this.wondoo = wondoo;
+    this.water = water;
+    this.sugar = sugar;
+    this.milk = milk;
+  }
+}
+let test = new makeCoffee("원두", "물", "설탕", "우유");
+console.log(test);
