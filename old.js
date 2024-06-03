3252d24d4f52aa01938260f207dc21d6ca5b2c47
dalramjwi <dalramjwi@gmail.com>
2024-06-03 15:33:44 +0900

    class를 활용한 객체 표기법

--- old.js
+++ old.js
@@ -4,3 +4,12 @@ function old(a, b) {
 }
 let test = new old(1, 2);
 console.log(test);
+
+class NewOld {
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+  }
+}
+let newStyle = new NewOld(1, 2);
+console.log(newStyle);
